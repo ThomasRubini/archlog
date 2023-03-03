5c15770cf351e14174b04226602521ec976fb804
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2023-03-03 10:42:14 +0100

    remove var_dump

--- control/CommentsPresenter.php
+++ control/CommentsPresenter.php
@@ -15,7 +15,6 @@ class CommentsPresenter {
     public function getAllCommentsHTML(){
         $content = '<h1> Comments received :</h1>  <ul>';
         foreach ($this->comments->getCommentsTxt() as $comment) {
-            var_dump($comment);
             $content .= ' <li>';
             $content .= '<a href="/annonces/index.php/post?ID='.$comment['ANNONCE_ID'].'">From this post</a>';
             $content .= '<p>From: ' . $comment['USER_LOGIN'] . '</p>';
